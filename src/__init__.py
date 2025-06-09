5a74928ac8e7a672f129c0ff1ed18e93903ee5ee
Stefano Blando <149769347+StefanoBlando@users.noreply.github.com>
2025-06-09 16:13:20 +0200

    Create __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,6 @@
+"""Custom Chatbot with RAG - Main package."""
+__version__ = "0.1.0"
+
+from .data_processor import CharacterDataProcessor
+from .chatbot_engine import ChatbotEngine
+from .interactive_chatbot import InteractiveChatbot
